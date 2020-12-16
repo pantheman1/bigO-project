4e9bba205014947db48d2c20d1affcfc0c352404
OByrnes <liviabyrnes@gmail.com>
2020-12-16 18:44:14 -0500

    bad two Sum

--- 03-two-sum.js
+++ 03-two-sum.js
@@ -1,10 +1,28 @@
 function badTwoSum(arr, targetSum) {
-  // Code goes here ...
+  for (let i= 0; i< arr.length; i++){
+    let el1 = arr[i]
+    for (let j= i+1; j < arr.length; j++){
+      let el2= arr[j]
+      if(el1+el2 === targetSum){
+        return true
+      }
+    }
+  }
+  return false
+
 }
+
+const arr = [0, 1, 5, 7];
+// console.log(badTwoSum(arr, 10)) // should be false
+// console.log(badTwoSum(arr, 6)); // => should be true
 
 
 function okayTwoSum1(arr, targetSum) {
-  // Code goes here ...
+  arr.sort()
+  let midIdx=Math.floor((arr.length/2))
+  let left = arr.slice(0,midIdx)
+  let right = arr.slice(midIdx+1)
+  if()
 }
 
 
